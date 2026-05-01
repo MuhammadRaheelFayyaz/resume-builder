cc4303daae66bf2516812fa580d511a5b01a9542
MuhammadRaheelFayyaz <raheelfayyaz0002@gmail.com>
2026-05-01 16:19:55 +0500

    update site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yourdomain.com'; // change to your actual domain
+  const baseUrl = 'https://mycvbuilder.info'; // change to your actual domain
   return [
     { url: `${baseUrl}/`, lastModified: new Date() },
     { url: `${baseUrl}/builder`, lastModified: new Date() },
